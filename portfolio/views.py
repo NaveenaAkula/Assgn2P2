52564b0aca684e740bd6fef499b27cee6b88f426
NaveenaAkula <jakula@unomaha.edu>
2020-07-30 00:01:50 -0500

    customeredit_delete_create

--- portfolio/views.py
+++ portfolio/views.py
@@ -45,64 +45,46 @@ def customer_list(request):
                   {'customers': customer})
 
 
-# @login_required
-# def customer_new(request):
-#     if request.method == "POST":
-#         form = CustomerForm(request.POST)
-#         if form.is_valid():
-#             customer = form.save(commit=False)
-#             customer.created_date = timezone.now()
-#             customer.save()
-#             customers = Customer.objects.filter(created_date__lte=timezone.now())
-#             return render(request, 'portfolio/customer_list.html',
-#                           {'customers': customers})
-#     else:
-#         form = CustomerForm()
-#         # print("Else")
-#     return render(request, 'portfolio/customer_new.html', {'form': form})
-#
-#
-# @login_required
-# def customer_edit(request, pk):
-#     customer = get_object_or_404(Customer, pk=pk)
-#     if request.method == "POST":
-#         # update
-#         form = CustomerForm(request.POST, instance=customer)
-#         if form.is_valid():
-#             customer = form.save(commit=False)
-#             customer.updated_date = timezone.now()
-#             customer.save()
-#             customer = Customer.objects.filter(created_date__lte=timezone.now())
-#             return render(request, 'portfolio/customer_list.html',
-#                           {'customers': customer})
-#     else:
-#         # edit
-#         form = CustomerForm(instance=customer)
-#     return render(request, 'portfolio/customer_edit.html', {'form': form})
-#
-#
-# @login_required
-# def customer_new(request):
-#     if request.method == "POST":
-#         form = CustomerForm(request.POST)
-#         if form.is_valid():
-#             customer = form.save(commit=False)
-#             customer.created_date = timezone.now()
-#             customer.save()
-#             customers = Customer.objects.filter(created_date__lte=timezone.now())
-#             return render(request, 'portfolio/customer_list.html',
-#                           {'customers': customers})
-#     else:
-#         form = CustomerForm()
-#         # print("Else")
-#     return render(request, 'portfolio/customer_new.html', {'form': form})
-#
-#
-# @login_required
-# def customer_delete(request, pk):
-#     customer = get_object_or_404(Customer, pk=pk)
-#     customer.delete()
-#     return redirect('portfolio:customer_list')
+@login_required
+def customer_new(request):
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.created_date = timezone.now()
+            customer.save()
+            customers = Customer.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'portfolio/customer_list.html',
+                          {'customers': customers})
+    else:
+        form = CustomerForm()
+        # print("Else")
+    return render(request, 'portfolio/customer_new.html', {'form': form})
+
+
+@login_required
+def customer_edit(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == "POST":
+        # update
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.updated_date = timezone.now()
+            customer.save()
+            customer = Customer.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'portfolio/customer_list.html',
+                          {'customers': customer})
+    else:
+        # edit
+        form = CustomerForm(instance=customer)
+    return render(request, 'portfolio/customer_edit.html', {'form': form})
+
+@login_required
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customer.delete()
+    return redirect('portfolio:customer_list')
 
 
 # @login_required
